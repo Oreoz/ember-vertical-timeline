58a433379598be3b14112ec57312c728bbc15a47
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2017-10-07 16:25:57 -0400

    RFC176 Compliant Imports

--- addon/components/timeline/timeline-block.js
+++ addon/components/timeline/timeline-block.js
@@ -1,7 +1,5 @@
-import Ember from 'ember';
+import Component from '@ember/component';
 import layout from '../../templates/components/timeline/timeline-block';
-
-const { Component } = Ember;
 
 export default Component.extend({
   layout,
--- tests/integration/components/timeline/timeline-image-test.js
+++ tests/integration/components/timeline/timeline-image-test.js
@@ -1,8 +1,6 @@
+import { run } from '@ember/runloop';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import Ember from 'ember';
-
-const { run } = Ember;
 
 moduleForComponent('timeline/timeline-image', 'Integration | Component | Timeline Image', {
   integration: true
